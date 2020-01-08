c95753027508e0c057c54ba35bf082ec24b2112d
krainbow96 <40452621+krainbow96@users.noreply.github.com>
2020-01-08 16:38:57 +0900

    Add files via upload

---
+++ Temp_Humi_Spreadsheet/Temp_Humi_Spreadsheet_arduino.ino
@@ -0,0 +1,97 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <DHT11.h>
+
+DHT11 dht11 (D2);
+
+const char* ssid = "commoncoding";    //your wifi network
+const char* password = "coding0317";     //yout wifi pasword
+const char* host = "script.google.com";
+const int httpsPort = 443;
+
+WiFiClientSecure client;
+
+const char* fingerprint = "46 B2 C3 44 9C 59 09 8B 01 B6 F8 BD 4C FB 00 74 91 2F EF F6";
+String GAS_ID = " ";     //spreadsheet id
+
+void setup() 
+{
+  
+  Serial.begin(115200); 
+  Serial.println();
+
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  client.setInsecure();
+}
+
+void loop() 
+{
+  int err;
+  float temp, humi;
+  if((err = dht11.read(humi, temp))==0)
+  {
+  Serial.print(temp);
+  Serial.print("     ");
+  Serial.println(humi);
+  }
+  sendData(temp, humi);
+ 
+}
+
+void sendData(int tem, int hum)
+{
+  Serial.print("connecting to ");
+  Serial.println(host);
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+
+  if (client.verify(fingerprint, host)) {
+  Serial.println("certificate matches");
+  } else {
+  Serial.println("certificate doesn't match");
+  }
+  String string_temperature =  String(tem, DEC); 
+  String string_humidity =  String(hum, DEC); 
+  String url = "/macros/s/" + GAS_ID + "/exec?temperature=" + string_temperature + "&humidity=" + string_humidity;
+  Serial.print("requesting URL: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+         "Host: " + host + "\r\n" +
+         "User-Agent: BuildFailureDetectorESP8266\r\n" +
+         "Connection: close\r\n\r\n");
+
+  Serial.println("request sent");
+  while (client.connected()) {
+  String line = client.readStringUntil('\n');
+  if (line == "\r") {
+    Serial.println("headers received");
+    break;
+  }
+  }
+  String line = client.readStringUntil('\n');
+  if (line.startsWith("{\"state\":\"success\"")) {
+  Serial.println("esp8266/Arduino CI successfull!");
+  } else {
+  Serial.println("esp8266/Arduino CI has failed");
+  }
+  Serial.println("reply was:");
+  Serial.println("==========");
+  Serial.println(line);
+  Serial.println("==========");
+  Serial.println("closing connection");
+} 
